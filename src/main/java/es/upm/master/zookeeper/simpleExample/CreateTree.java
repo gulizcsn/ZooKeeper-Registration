544945c66edbd99e836ac3d284493651a9a65091
ssalgadom88 <ssalgado@outlook.com>
2017-10-19 02:20:41 +0200

    Adding backup

---
+++ src/main/java/es/upm/master/zookeeper/simpleExample/CreateTree.java
@@ -0,0 +1,38 @@
+package es.upm.master.zookeeper.simpleExample;
+
+import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooDefs;
+import org.apache.zookeeper.ZooKeeper;
+public class CreateTree {
+
+    public void constructTree(ZooKeeper zookeeperIns) throws KeeperException, InterruptedException {
+
+        //create znode
+        //zoo.create("/test", "znode".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+        //create znode sequential
+        //zoo.create("/test/sequential", "znode_sequential".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT_SEQUENTIAL);
+        //create znode ephemereal
+        //zoo.create("/test/ephemeral", "znode_ephemeral".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL);
+
+        zookeeperIns.create("/System", "znode".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+        //create znode sequential
+        zookeeperIns.create("/System/Request", "znode".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+        //create znode sequential*/
+        zookeeperIns.create("/System/Request/Enroll", "znode".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+        //create znode sequential
+        zookeeperIns.create("/System/Request/Quit", "znode".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+        //create znode sequential
+        zookeeperIns.create("/System/Registry", "znode".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+        //create znode sequential
+    }
+ public void destroyTree(ZooKeeper zookeeperIns) throws KeeperException, InterruptedException {
+
+
+        zookeeperIns.close();
+
+    }
+}
---
+++ src/main/java/es/upm/master/zookeeper/simpleExample/Test.java
@@ -0,0 +1,55 @@
+package es.upm.master.zookeeper.simpleExample;
+
+import java.io.IOException;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooDefs;
+import org.apache.zookeeper.ZooKeeper;
+
+import es.upm.master.zookeeper.SimpleWatcher;
+
+import org.apache.zookeeper.Watcher.Event.KeeperState;
+
+public class Test {
+	
+	
+	public static void main(String[] args) throws IOException, InterruptedException, KeeperException {
+		
+		
+		String host = "localhost:2181";
+		int sessionTimeout = 3000;
+		final CountDownLatch connectionLatch = new CountDownLatch(1);
+		 
+		//create a connection
+		ZooKeeper zoo = new ZooKeeper(host, sessionTimeout , new Watcher() {
+
+			@Override
+			public void process(WatchedEvent we) {
+
+				if(we.getState() == KeeperState.SyncConnected){
+					connectionLatch.countDown();
+				}
+
+			}
+		});
+
+		connectionLatch.await(10, TimeUnit.SECONDS);
+
+		CreateTree tree = new CreateTree();
+		tree.constructTree(zoo);
+
+		ZKManager manager = new ZKManager();
+        //menu for deciding what to do inside class manager- we should move it
+		manager.ZKManager(zoo);
+
+
+
+	}
+
+
+}
